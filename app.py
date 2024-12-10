dc9becfb1918a759daf0d6538a57887d23bab751
erstcl <e.starikov@edu.centraluniversity.ru>
2024-12-10 12:21:40 +0300

    Add testing route

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import jsonify
+from weather_api import get_weather
+from weather_model import check_bad_weather
 
 app = Flask(__name__)
 
@@ -6,5 +9,13 @@ app = Flask(__name__)
 def index():
     return "Weather Route Forecast Service"
 
+@app.route('/test/<latitude>/<longitude>')
+def test_weather(latitude, longitude):
+    weather_data = get_weather(latitude, longitude)
+    result = check_bad_weather(weather_data)
+    return jsonify(result)
+
+
 if __name__ == "__main__":
     app.run(debug=True)
+    
